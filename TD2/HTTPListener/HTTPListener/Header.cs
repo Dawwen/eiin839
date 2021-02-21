c8caae5221bada475d7977d89deeb851983d0119
Olivier Doussaud <olivier.dousaud@etu.unice.fr>
2021-02-21 12:51:05 +0100

    Add Exo1

---
+++ TD2/HTTPListener/HTTPListener/Header.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicHTTPlistener
+{
+    class Header
+    {
+        System.Collections.Specialized.NameValueCollection head;
+        public Header(System.Collections.Specialized.NameValueCollection headers)
+        {
+            head = headers;
+        }
+
+        public string getHeader()
+        {
+            return head.ToString();
+        }
+    }
+}
