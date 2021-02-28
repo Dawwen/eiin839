c686fc8aa57e3aab5204aeb30b397bf57444d50b
Olivier Doussaud <olivier.dousaud@etu.unice.fr>
2021-02-28 18:45:41 +0100

    Update the HtppListener

--- TD2/HTTPListener/HTTPListener/Header.cs
+++ TD2/HTTPListener/HTTPListener/Header.cs
@@ -17,7 +17,25 @@ namespace BasicHTTPlistener
 
         public string getHeader()
         {
-            return head.ToString();
+            Console.WriteLine(head.ToString());
+            Console.WriteLine(head.Keys.ToString());
+            Console.WriteLine(HttpRequestHeader.AcceptEncoding.ToString());
+            string output = "<HTML><BODY>\n";
+
+            output += "<H1>The request was received.</H1>\n";
+
+            output += "The connexionType is: "  + head.Get(HttpRequestHeader.Connection.ToString()) + "<br>\n";
+            output += "The host is: " + head.Get(HttpRequestHeader.Host.ToString()) + "<br>\n";
+
+            /* Should use these line but postmann send User-Agent and not UserAgent as it is in the HttpRequestHeader same for Accept-Encoding and AcceptEncoding
+            output += "The application used is: " + head.Get(HttpRequestHeader.UserAgent.ToString()) + "<br>\n";
+            output += "It accept these kind of encoding: " + head.Get(HttpRequestHeader.AcceptEncoding.ToString()) + "<br>\n"; */
+            
+            output += "It accept these kind of encoding: " + head.Get("Accept-Encoding") + "<br>\n";
+            output += "The application used is: " + head.Get("User-Agent") + "<br>\n";
+            
+            output += "</BODY></HTML>";
+            return output;
         }
     }
 }
